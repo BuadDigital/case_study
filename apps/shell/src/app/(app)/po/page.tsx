dcb03e4ef57bc44e83e73548735ade65a699b9eb
OMR <omar.bamusa.888@gmail.com>
2026-07-15 11:45:36 +0300

    تجربة: تطبيق أحجام ونوع خط مرفق po_list على شاشة قائمة أوامر العمل فقط
    نطاق معزول عبر .po-list-type دون التأثير على بقية الشاشات.

--- apps/shell/src/app/(app)/po/page.tsx
+++ apps/shell/src/app/(app)/po/page.tsx
@@ -1,10 +1,13 @@
 import { Suspense } from "react";
 import { PoListView } from "@case-study/mfe";
+import "./po-list-typography.css";
 
 export default function PoPage() {
   return (
-    <Suspense fallback={null}>
-      <PoListView />
-    </Suspense>
+    <div className="po-list-type">
+      <Suspense fallback={null}>
+        <PoListView />
+      </Suspense>
+    </div>
   );
 }
